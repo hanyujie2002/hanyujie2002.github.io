b06b616731b1023ecb7e8d77e27e087831d565b1
hanyujie2002 <github-actions@github.com>
2023-09-07 04:42:30 +0000

    Deploy at Thu Sep  7 04:42:30 UTC 2023

---
+++ _nuxt/ProseH1.c94faf9e.js
@@ -0,0 +1,1 @@
+import{_ as o,o as r,c as s,q as t}from"./entry.8a49569f.js";const c={};function n(e,a){return r(),s("h1",null,[t(e.$slots,"default")])}const _=o(c,[["render",n]]);export{_ as default};
